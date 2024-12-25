9e0113fa795f0714fdd845bc8b021bdef1e45c71
nevaldurmaz <neval.durmaz@icloud.com>
2024-12-25 16:54:55 +0300

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import restaurantReducer from "./reducers/restaurantReducer";
+import cartReducer from "./reducers/cartReducer";
+import { thunk } from "redux-thunk";
+
+const rootReducer = combineReducers({
+  restaurantReducer,
+  cartReducer,
+});
+
+// apply middleware herhangi arayızılımı redux'a dahil etmeye yarar.
+// bi burada thunk'ı dahil etmek için kullanıyoruz
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
